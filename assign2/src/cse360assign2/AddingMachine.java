6a410909c86100db0da40526adbbfbe735d93bc6
Steven <Steven@StSitu>
2019-06-18 23:28:48 -0700

    Real commit now?

---
+++ assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,52 @@
+package cse360assign2;
+
+/**
+ * @author Steven
+ * Attempt 1
+ */
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * 
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * @return
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+		
+	/**
+	 *
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * 
+	 */
+	public void clear() {
+	
+	}
+}
